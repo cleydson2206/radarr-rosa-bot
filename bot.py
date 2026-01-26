c31527d3f32d9cb5a94ec0594d1a275dbd7534cb
cleydson2206 <cleydsonsoares020@gmail.com>
2026-01-26 00:18:37 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,6 +44,8 @@ def enviar_sinal_grupo(hora_rosa):
     mensagem = (
         "🌹 <b>ROSA 10x+ DETECTADO</b>\n\n"
         f"⏰ <b>Horário da rosa:</b> {hora_rosa}\n"
+        "ENTRAR NO MINUTO"
+        "1 MINUTO ANTES E 1 MINUTOS DEPOIS"
         "🎯 <b>ZONAS QUENTES:</b>\n"
         f"🎯 {zonas[0]}\n"
         f"🎯 {zonas[1]}\n"
